9dd649f793567045217656f793a371056572b203
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-04 13:53:39 +0530

    Udpated code

--- manifests/groupresourcetype.pp
+++ manifests/groupresourcetype.pp
@@ -1,41 +1,62 @@
-class basicresourcetypes::groupresourcetype {
-  $users=['tom', 'dick', 'harry']
-  $value=1050
-  user{$users:
-    ensure         => present,
-    comment        => 'this is a test user for testing groups',
-    #uid            => [10050, 100051,100052],
-    allowdupe      => false,
-    membership     => minimum,
-    purge_ssh_keys => true,
-    shell          => '/bin/bash',
-  }
-  group{ 'group1 - members - [tom, dick]':
-    ensure          => present,
-    name            => 'group1',
-    gid             => 1015,
-    allowdupe       => false,
-    auth_membership => false,
-    members         => ['tom', 'dick'],
-    system          => false,
-  }
-  group { 'group2 - members - [dick, harry]':
-    ensure          => present,
-    name            => 'group2',
-    gid             => 1016,
-    allowdupe       => false,
-    auth_membership => false,
-    members         => ['dick','harry'],
-    system          => false,
-  }
-  group { 'group3 - members - [harry,tom]':
-    ensure          => present,
-    name            => 'group3',
-    gid             => 1017,
-    allowdupe       => false,
-    auth_membership => false,
-    members         => ['harry', 'tom'],
-    system          => false,
-  }
-  User[$users] ~> [Group['group1 - members - [tom, dick]'], Group['group2 - members - [dick, harry]'], Group['group3 - members - [harry,tom]']]
+class basicresourcetypes::groupresourcetype(
+  Array[Hash[String, Variant[String, Integer]]] $users,
+) {
+    $users.each |$user_hash| {
+      user { $user_hash['name']:
+        ensure         => present,
+        comment        => "Test user :: ${user_hash['name']}",
+        uid            => $user_hash['uid'],
+        allowdupe      => false,
+        membership     => minimum,
+        groups         => $user_hash['groups'],
+        purge_ssh_keys => true,
+        shell          => '/bin/bash',
+      }
+    }
+    group { 'group1 - members - [test-user-1, test-user-2]':
+      ensure    => present,
+      name      => 'group1',
+      gid       => 1015,
+      allowdupe => false,
+      #members   => ['tom', 'dick'],
+      system    => false,
+    }
+    group { 'group2 - members - [test-user-2, test-user-3]':
+      ensure    => present,
+      name      => 'group2',
+      gid       => 1016,
+      allowdupe => false,
+      system    => false,
+    }
+    group { 'group3 - members - [test-user-3, test-user-1]':
+      ensure    => present,
+      name      => 'group3',
+      gid       => 1017,
+      allowdupe => false,
+      system    => false,
+    }
+    Group['group1', 'group2', 'group3'] ~> User['test-user-1', 'test-user-2', 'test-user-3']
+}
+
+Class { 'groupresourcetype':
+  users => [
+    {
+      name   => 'test-user-1',
+      uid    => 3001,
+      groups => 'group3',
+      gid    => 1015,
+    },
+        {
+      name   => 'test-user-2',
+      uid    => 3001,
+      groups => 'group1',
+      gid    => 1016
+    },
+        {
+      name   => 'test-user-3',
+      uid    => 3001,
+      groups => 'group2',
+      gid    => 1017
+    }
+  ]
 }
