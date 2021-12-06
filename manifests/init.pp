fe8c9dfe9302dfe0a473121a11a3e637c86b9b01
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-06 12:28:39 +0530

    Udpated code

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,4 +3,5 @@ class basicresourcetypes {
   # include basicresourcetypes::execresourcetype
   # include basicresourcetypes::fileresourcetype
   # include basicresourcetypes::resourcesresourcetype
+  # include basicresourcetypes::serviceresourcetype
 }
