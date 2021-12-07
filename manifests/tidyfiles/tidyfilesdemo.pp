ff314b5432422ecf838ab130c62a39f04d3824e8
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-07 12:39:07 +0530

    Udpated code

--- manifests/tidyfiles/tidyfilesdemo.pp
+++ manifests/tidyfiles/tidyfilesdemo.pp
@@ -2,8 +2,8 @@ class basicresourcetypes::tidyfiles::tidyfilesdemo{
   tidy { 'tidytmp1-dir':
     path    => '/tmp/tidy-tmp-1',
     matches => ['*tmp*','*pdf*','*temp*', '*.txt*'],
-    size    => '1k',
-    recurse => false,
+    size    => 1,
+    recurse => true,
   }
   tidy { 'tidytmp2-dir':
     path      => '/tmp/tidy-tmp-2',
@@ -11,12 +11,12 @@ class basicresourcetypes::tidyfiles::tidyfilesdemo{
     age       => '1s',
     type      => 'atime',
     max_files => 50,
-    recurse   => false,
+    recurse   => true,
   }
   tidy { 'tidytmp3-dir':
     path    => '/tmp/tidy-tmp-3',
     matches => ['*tmp*', '*temp*', '*txt*'],
-    size    => '1k',
+    size    => 1,
     age     => '1s',
     type    => 'atime',
     recurse => true,
@@ -25,6 +25,6 @@ class basicresourcetypes::tidyfiles::tidyfilesdemo{
   tidy { 'tidytmp4-dir':
     path    => '/tmp/tmp-dir-4',
     matches => ['*pdf*'],
-    recurse => false,
+    recurse => true,
   }
 }
