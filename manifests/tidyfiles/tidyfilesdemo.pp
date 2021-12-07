2d46b3c89403d1bf0fa4cfa876b75f81e112412a
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-07 12:40:09 +0530

    Udpated code

--- manifests/tidyfiles/tidyfilesdemo.pp
+++ manifests/tidyfiles/tidyfilesdemo.pp
@@ -2,7 +2,7 @@ class basicresourcetypes::tidyfiles::tidyfilesdemo{
   tidy { 'tidytmp1-dir':
     path    => '/tmp/tidy-tmp-1',
     matches => ['*tmp*','*pdf*','*temp*', '*.txt*'],
-    size    => 1,
+    size    => '1k',
     recurse => true,
   }
   tidy { 'tidytmp2-dir':
@@ -16,7 +16,7 @@ class basicresourcetypes::tidyfiles::tidyfilesdemo{
   tidy { 'tidytmp3-dir':
     path    => '/tmp/tidy-tmp-3',
     matches => ['*tmp*', '*temp*', '*txt*'],
-    size    => 1,
+    size    => '1k',
     age     => '1s',
     type    => 'atime',
     recurse => true,
