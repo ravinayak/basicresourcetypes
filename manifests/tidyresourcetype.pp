5cd89f3a9eab9b18d347591c19174f375dd1ae45
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-07 13:03:56 +0530

    Udpated code

--- manifests/tidyresourcetype.pp
+++ manifests/tidyresourcetype.pp
@@ -14,5 +14,36 @@ class basicresourcetypes::tidyresourcetype {
     stage => 'main',
   }
   Class['basicresourcetypes::tidyfiles::filecreationdeclaration','basicresourcetypes::tidyfiles::filecopy' ] -> Exec['wait for 2s']
-  Exec['wait for 2s'] -> Class['basicresourcetypes::tidyfiles::tidyfilesdemo']
+  #Exec['wait for 2s'] -> Class['basicresourcetypes::tidyfiles::tidyfilesdemo']
+    tidy { 'tidytmp1-dir':
+    path    => '/tmp/tidy-tmp-1',
+    matches => ['*.tmp*','*.pdf*','*.temp*', '*.txt*'],
+    size    => '1k',
+    age     => '1s',
+    type    => 'atime',
+    recurse => true,
+  }
+  tidy { 'tidytmp2-dir':
+    path      => '/tmp/tidy-tmp-2',
+    matches   => ['*.tmp*','*.pdf*'],
+    size      => '1k',
+    age       => '1s',
+    type      => 'atime',
+    max_files => 50,
+    recurse   => true,
+  }
+  tidy { 'tidytmp3-dir':
+    path    => '/tmp/tidy-tmp-3',
+    matches => ['*.tmp*', '*.temp*', '*.txt*'],
+    size    => '1k',
+    age     => '1s',
+    type    => 'atime',
+    recurse => true,
+    rmdirs  => true,
+  }
+  tidy { 'tidytmp4-dir':
+    path    => '/tmp/tidy-tmp-4',
+    matches => ['*.pdf*'],
+    recurse => true,
+  }
 }
