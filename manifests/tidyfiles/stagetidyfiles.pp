7510ad2d3b45dbc4534f505c92d1982ffc7cbc7b
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-07 10:30:22 +0530

    Udpated code

--- manifests/tidyfiles/stagetidyfiles.pp
+++ manifests/tidyfiles/stagetidyfiles.pp
@@ -2,9 +2,6 @@ class basicresourcetypes::tidyfiles::stagetidyfiles {
   stage { 'filecreationdeclaration':
     before => Stage['Main'],
   }
-  ->stage { 'filecreation':
-    before => Stage['Main'],
-  }
   ->stage { 'filecopy':
     before => Stage['Main'],
   }
