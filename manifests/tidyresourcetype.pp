6148a499a6044a0cdf08a20a41a11689739b8d52
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-07 16:41:44 +0530

    Udpated code

--- manifests/tidyresourcetype.pp
+++ manifests/tidyresourcetype.pp
@@ -1,9 +1,17 @@
 class basicresourcetypes::tidyresourcetype {
-  include basicresourcetypes::tidyfiles::stagetidyfiles
+  stage { 'filecreationdeclaration':
+    before => Stage['filecopy'],
+  }
+  stage { 'filecopy':
+    before => Stage['main'],
+  }
   Class {'basicresourcetypes::tidyfiles::filecreationdeclaration':
     stage => 'filecreationdeclaration',
   }
   Class {'basicresourcetypes::tidyfiles::filecopy':
     stage  => 'filecopy',
+  }
+  Class { 'basicresourcetypes::tidyfiles::tidyresourcesdescription':
+    stage => 'main',
   }
 }
