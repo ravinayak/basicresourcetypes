a62570eaf0eeb36d3e2588636b91b881c73f0644
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-08 07:56:05 +0530

    notifyresource added

--- manifests/notifyresourcetype.pp
+++ manifests/notifyresourcetype.pp
@@ -5,5 +5,8 @@ class basicresourcetypes::notifyresourcetype(
     name     => 'dummy-message-notify',
     message  => $message,
     withpath => false,
+    # /Stage[main]/Basicresourcetypes::Notifyresourcetype/Notify[dummy-message]/message: This is the full path of the object, which 
+    # is displayed when we set withpath to true
+    # Notice: This is a notify demo: This is displayed when withpath is set to false
   }
 }
